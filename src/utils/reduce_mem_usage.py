9056ed51dd4bf053dd46dd2d5eeacf5e70bd90fa
yota-p <930713yh@gmail.com>
2020-01-10 23:56:10 +0900

    Change print in reduce_mem_usage into logger

--- src/utils/reduce_mem_usage.py
+++ src/utils/reduce_mem_usage.py
@@ -3,37 +3,6 @@ from utils.mylog import timer
 from logging import getLogger
 logger = getLogger('main')
 
-'''
-@timer
-def reduce_mem_usage(df):
-    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
-    start_mem = df.memory_usage(deep=True).sum() / 1024**2
-    for col in df.columns:
-        col_type = df[col].dtypes
-        if col_type in numerics:
-            c_min = df[col].min()
-            c_max = df[col].max()
-            if str(col_type)[:3] == 'int':
-                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-                    df[col] = df[col].astype(np.int8)
-                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-                    df[col] = df[col].astype(np.int16)
-                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-                    df[col] = df[col].astype(np.int32)
-                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)
-            else:
-                c_prec = df[col].apply(lambda x: np.finfo(x).precision).max()
-                if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max and c_prec == np.finfo(np.float32).precision:
-                    df[col] = df[col].astype(np.float32)
-                else:
-                    df[col] = df[col].astype(np.float64)
-    end_mem = df.memory_usage(deep=True).sum() / 1024**2
-    logger.debug(
-        f'Mem. usage decreased to {end_mem:5.2f} Mb ({(100 * (start_mem - end_mem) / start_mem):.1f}% reduction)')
-    return df
-'''
-
 
 @timer
 def reduce_mem_usage(df):
@@ -65,11 +34,8 @@ def reduce_mem_usage(df):
                     df[col] = df[col].astype(np.float32)
                 else:
                     df[col] = df[col].astype(np.float64)
-        #else:
-        #    df[col] = df[col].astype('category')
-
     end_mem = df.memory_usage().sum() / 1024**2
-    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
-    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+    logger.debug('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    logger.debug('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
 
     return df
