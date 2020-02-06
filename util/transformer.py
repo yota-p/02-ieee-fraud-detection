96f05dd22da5b52d9630085d32e945b413d9df63
yota-p <930713yh@gmail.com>
2020-02-06 21:14:25 +0900

    not to resample .small data

--- util/transformer.py
+++ util/transformer.py
@@ -22,39 +22,39 @@ class Transformer:
         '''
         Create features and return datas for training
         '''
-        # check if output exists
+        # check if output exists, read from file
         if is_latest([transformed_train_path, transformed_test_path]):
             logger.debug('All files existed. Skip transforming.')
             train = pd.read_pickle(str(transformed_train_path))
             test = pd.read_pickle(str(transformed_test_path))
             logger.debug(f'Loaded train.shape: {train.shape}')
             logger.debug(f'Loaded test.shape:  {test.shape}')
+            return train, test
 
-        else:
-            # Get key columns
-            factory = FeatureFactory()
-            raw = factory.create('raw')
-            train_raw, test_raw = raw.create_feature()
-            train = train_raw[['TransactionID']]
-            test = test_raw[['TransactionID']]
+        # Get key columns
+        factory = FeatureFactory()
+        raw = factory.create('raw')
+        train_raw, test_raw = raw.create_feature()
+        train = train_raw[['TransactionID']]
+        test = test_raw[['TransactionID']]
 
-            # For column: create features
-            for namespace in features:
-                feature = factory.create(namespace)
-                train_feature, test_feature = feature.create_feature()
+        # For column: create features
+        for namespace in features:
+            feature = factory.create(namespace)
+            train_feature, test_feature = feature.create_feature()
 
-                # check if row # match before merge
-                if not len(train.index) == len(train_feature.index):
-                    raise TypeError(f'Unable to merge: length of train and feature_train does not match.')
-                if not len(test.index) == len(test_feature.index):
-                    raise TypeError(f'Unable to merge: length of test and feature_test does not match.')
+            # check if row # match before merge
+            if not len(train.index) == len(train_feature.index):
+                raise TypeError(f'Unable to merge: length of train and feature_train does not match.')
+            if not len(test.index) == len(test_feature.index):
+                raise TypeError(f'Unable to merge: length of test and feature_test does not match.')
 
-                train = pd.merge(train, train_feature, how='left', on='TransactionID')
-                test = pd.merge(test, test_feature, how='left', on='TransactionID')
-                del feature, train_feature, test_feature
+            train = pd.merge(train, train_feature, how='left', on='TransactionID')
+            test = pd.merge(test, test_feature, how='left', on='TransactionID')
+            del feature, train_feature, test_feature
 
-            train = train.sort_values(by=['TransactionDT'])
-            test = test.sort_values(by=['TransactionDT'])
+        train = train.sort_values(by=['TransactionDT'])
+        test = test.sort_values(by=['TransactionDT'])
 
         if USE_SMALL_DATA:
             frac = 0.001
