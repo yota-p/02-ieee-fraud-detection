56be9ffde6d2f34ccf07f63063ee87c533045986
yota-p <930713yh@gmail.com>
2020-02-11 19:45:49 +0900

    Update option.py

--- util/option.py
+++ util/option.py
@@ -2,6 +2,13 @@ from argparse import ArgumentParser
 
 
 def parse_option():
+     '''
+     Get optional variables from command line arguments		
+     --		
+     References		
+     https://qiita.com/taashi/items/400871fb13df476f42d2		
+     https://qiita.com/kzkadc/items/e4fc7bc9c003de1eb6d0		
+     '''
     argparser = ArgumentParser()
 
     argparser.add_argument('version',
